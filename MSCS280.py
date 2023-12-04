596fd1116431c5dbb0922fc24ef365671c1570dd
Stephen C <83474041+MintyMika@users.noreply.github.com>
2023-12-04 17:14:15 -0600

    Update MSCS280.py

--- MSCS280.py
+++ MSCS280.py
@@ -9,8 +9,11 @@
 
 import networkx as nx
 from networkx.algorithms import bipartite
+# import planarity testing library
+from networkx.algorithms.planarity import check_planarity
 from itertools import combinations, product
 import matplotlib.pyplot as plt
+import random
 
 # First we will create K5, K3,3, and K4 as graphs to test against.
 K5 = nx.Graph()
@@ -83,8 +86,77 @@ def isPlanar(graph):
             
     return True
     
+def graphFromAdjacency(adjacency_matrix):
+    # This function will create a graph from an adjacency matrix.
+    # The adjacency matrix will be a list of lists.
+    # The adjacency matrix will be a square matrix.
+    # The adjacency matrix will be symmetric.
+
+    nodes = []
+    edges = []
+
+    for i in range(len(adjacency_matrix)):
+        nodes.append(i+1)
+        for j in range(len(adjacency_matrix[i])):
+            if adjacency_matrix[i][j] == 1:
+                edges.append((i+1, j+1))
+    
+    graph = nx.Graph()
+    graph.add_nodes_from(nodes)
+    graph.add_edges_from(edges)
+
+    return graph
+
+
+
+def randomAdjacencyMatrix(n):
+    # This function will create a random adjacency matrix of size n.
+    # The adjacency matrix will be a list of lists.
+    # The adjacency matrix will be a square matrix.
+    # The adjacency matrix will be symmetric.
+
+    # First we will create an empty adjacency matrix.
+
+    adjacency_matrix = []
+
+    for i in range(n):
+        adjacency_matrix.append([])
+        for j in range(n):
+            adjacency_matrix[i].append(0)
+    
+    # Now we will randomly fill in the adjacency matrix.
+
+    for i in range(n):
+        for j in range(i+1, n):
+            adjacency_matrix[i][j] = adjacency_matrix[j][i] = random.randint(0,1)
+    
+    return adjacency_matrix
+    
+
 def main():
-    graphFile = open("Graphs.txt", "r")
+    #graphFile = open("Graphs.txt", "r")
 
     # TODO: Read in the graphs from the file and determine if they are planar.
+    # drawGraph(K5)
+    # drawGraph(K5var)
+    # drawGraph(K33)
+    # drawGraph(K33var)
+    # drawGraph(K4)
+    # print("K5 is planar: " + str(isPlanar(K5)))
+    # print("K5var is planar: " + str(isPlanar(K5var)))
+    # print("K33 is planar: " + str(isPlanar(K33)))
+    # print("K33var is planar: " + str(isPlanar(K33var)))
+    # print("K4 is planar: " + str(isPlanar(K4)))
 
+
+    for i in range(3):
+        # Create random graphs, display them, and determine if they are planar.
+        randint = random.randint(5, 10)
+        randomGraph = graphFromAdjacency(randomAdjacencyMatrix(randint))
+        print("My random graph is planar: " + str(isPlanar(randomGraph)))
+        drawGraph(randomGraph)
+
+    #graphFile.close()
+
+if __name__ == "__main__":
+    main()
